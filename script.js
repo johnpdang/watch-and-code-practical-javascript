234f407bd425f420664f5d5dd7c959aeff03610a
John <johnpdang@gmail.com>
2019-05-22 07:59:26 -0700

    displayTodos should tell you if .todos is empty

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log("My Todos", this.todos);
+    if (this.todos.length === 0) {
+      console.log("Your todo list is empty!");
+    } else {
+      console.log("My Todos:");
+      for (var i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].todoText);
+      }
+    }
   },
   addTodo: function(todoText) {
     this.todos.push({
@@ -10,12 +17,18 @@ var todoList = {
     });
     this.displayTodos();
   },
-  changeTodo: function(position, newValue) {
-    this.todos[position] = newValue;
+  changeTodo: function(position, todoText) {
+    // this.todos[position] = newValue;
+    this.todos[position].todoText = todoText;
     this.displayTodos();
   },
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
     this.displayTodos();
   }
 };

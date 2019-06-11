7d3af1497f655a890990c9fa5dfd62ea54c542de
John Dang <john.dang@trainadesign.com>
2019-06-11 10:26:31 -0700

    more on refactoring

--- script.js
+++ script.js
@@ -61,16 +61,6 @@ var todoList = {
   }
 };
 
-// var displayTodosButton = document.getElementById("displayTodosButton");
-// var toggleAllButton = document.getElementById("toggleAllButton");
-
-// displayTodosButton.addEventListener("click", function() {
-//   todoList.displayTodos();
-// });
-// toggleAllButton.addEventListener("click", function() {
-//   todoList.toggleAll();
-// });
-
 var handlers = {
   displayTodos: function() {
     todoList.displayTodos();

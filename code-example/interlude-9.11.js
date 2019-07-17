030785f26401f63a9abce6ad5aefd9660de616f6
John <johnpdang@gmail.com>
2019-07-17 11:39:30 -0700

    version 10 requirements

---
+++ code-example/interlude-9.11.js
@@ -0,0 +1,14 @@
+/* ==============================
+higher order functions
+============================== */ 
+// functions that accept other functions
+// Enhance the behavior of other functions
+runwithdebugger
+setTimeout
+forEach
+addEventListener
+
+/* ==============================
+callback functions
+============================== */
+// the function that are passed into higher order functions

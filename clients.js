ecc011799eb8b4ae5ddcecc1e8df37773148d845
John Dang <john.dang@trainadesign.com>
2019-11-21 14:25:55 -0800

    view.displayTodos should use forEach

--- clients.js
+++ clients.js
@@ -26,12 +26,11 @@ var todoList = {
         completedTodos++;
       }
     });
-
     this.todos.forEach(function(todo) {
-// Case 1: If everything’s true, make everything false.
+      // Case 1: If everything’s true, make everything false.
       if(completedTodos === totalTodos) {
         todo.completed = false;
-// Case 2: Otherwise, make everything true.
+      // Case 2: Otherwise, make everything true.
       } else {
         todo.completed = true;
       }
@@ -75,9 +74,8 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for (var i = 0; i < todoList.todos.length; i++) {
+    todoList.todos.forEach(function(todo, position) {
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
       if (todo.completed === true) {
@@ -86,11 +84,12 @@ var view = {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
 
-      todoLi.id = i;
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
-    }
+    }, this);
+
   },
   createDeleteButton: function() {
     var deleteButton = document.createElement('button');
